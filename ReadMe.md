31c9e6e55f88b677c7563cfab9a939fd6985fdd4
Nick <nmcarducci@gmail.com>
2022-09-26 19:53:28 -0400

    ok

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,5 @@
 `npm install mastercard-locations --save`
 
 trying to rollup the SDK dependency specifically
+
+# [Can I use axios in a worker?](https://community.cloudflare.com/t/can-i-use-axios-in-a-worker/168139)
--- src/index.mjs
+++ src/index.mjs
@@ -74,7 +74,7 @@ function UseDependency() {
     var MasterCardAPI = locations.MasterCardAPI;
 
     //var consumerKey = "your consumer key";   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
-    var keyStorePath = p12;//"path to your .p12 private key file"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
+    var keyStorePath = p12;// service worker envar "path to your .p12 private key file"; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
     var keyAlias = "keyalias";   // For production: change this to the key alias you chose when you created your production key
     var keyPassword = "keystorepassword";   // For production: change this to the key alias you chose when you created your production key
 
